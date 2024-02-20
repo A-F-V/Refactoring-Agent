aaf1211fa1c8c66787d8a448f4fc38b2a78bcc8d
A-F-V <faracea2000@gmail.com>
2024-02-20 20:37:08 +0000

    Update execution.py

--- src/execution.py
+++ src/execution.py
@@ -1,8 +1,4 @@
 import json
-from nis import cat
-import re
-import sys
-from tabnanny import verbose
 from typing import Dict, List
 from langchain_openai import ChatOpenAI
 from langchain_core.utils.function_calling import convert_to_openai_function
